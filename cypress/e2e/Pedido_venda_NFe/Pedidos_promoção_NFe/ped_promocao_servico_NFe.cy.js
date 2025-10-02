30f310d81853d49d135b2868ef2ecaa864b391be
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-10-02 19:54:09 -0300

    refactor: Replace ThrowDelivery with combined ThrowDelivery and ThrowAssembly imports across multiple test files

--- cypress/e2e/Pedido_venda_NFe/Pedidos_promoção_NFe/ped_promocao_servico_NFe.cy.js
+++ cypress/e2e/Pedido_venda_NFe/Pedidos_promoção_NFe/ped_promocao_servico_NFe.cy.js
@@ -3,7 +3,7 @@ import { ValidateBalance } from '../../../../pages/para_pedidos/saldo/validar_sa
 import { Product } from '../../../../pages/produtos/prd_normal.js'
 import { Service } from '../../../../pages/para_pedidos/servicos/valida_servicos_adicionados.js'
 import { AdvanceNormal } from '../../../../pages/para_pedidos/botoes/avancar/avancar_normal.js'
-import { ThrowDelivery } from '../../../../pages/para_pedidos/entrega/tirar_entrega.js'
+import { ThrowDelivery, ThrowAssembly } from '../../../../pages/para_pedidos/entrega/tirar_entrega_montagem.js'
 import { ReceiptPromotion } from '../../../../pages/para_pedidos/processos/processo_recebimento_promo.js'
 import { Promotion } from '../../../../pages/para_pedidos/promocao/promocao.js'
 import { TicketPrestamista } from '../../../../pages/para_pedidos/validar_tela/prestamista.js'
--- pages/para_pedidos/entrega/tirar_entrega_montagem.js
+++ pages/para_pedidos/entrega/tirar_entrega_montagem.js
@@ -1,3 +1,44 @@
+export class ThrowDelivery {
+  //Arrasta botão de Retirada / Entrega do primeiro produto.
+  static freightFirst() {
+    cy.get('.valor.flex-gt-sm-50 > .md-checked > .md-container')
+      .scrollIntoView()
+      .wait(500)
+      .should('be.visible')
+      .and('not.be.disabled');
+    cy.get('.valor.flex-gt-sm-50 > .md-checked > .md-container > .md-thumb-container > .md-thumb')
+      .should('be.visible')
+      .and('not.be.disabled');
+    cy.get('.valor.flex-gt-sm-50 > .md-checked > .md-label')
+      .should('be.visible')
+      .and('not.be.disabled')
+      .and('have.text', ' Retirada / Entrega ')
+      .click({ force: true });
+  }
+
+  //Arrasta botão de Retirada / Entrega do segundo produto.
+  static freightSecond() {
+    cy.get('.valor.flex-gt-sm-50 > .md-checked > .md-label')
+      .scrollIntoView()
+      .wait(300)
+      .should('be.visible')
+      .and('not.be.disabled')
+      .and('have.text', ' Retirada / Entrega ')
+      .click({ force: true });
+  }
+
+  //Arrasta botão de Retirada / Entrega do terceiro produto.
+  static freightThird() {
+    cy.get('.valor.flex-gt-sm-50 > .md-checked > .md-label')
+      .scrollIntoView()
+      .wait(300)
+      .should('be.visible')
+      .and('not.be.disabled')
+      .and('have.text', ' Retirada / Entrega ')
+      .click({ force: true });
+  }
+}
+
 export class ThrowAssembly {
   //Arrasta botão de Montagem do primeiro produto.
   static fisrt() {
